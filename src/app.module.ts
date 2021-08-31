434b522ef5be8051284a2f4f6038d4860277909e
Alex Sandri <me@alexsandri.com>
2021-08-31 09:48:13 +0200

    install passport and jwt modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { Config } from './config/Config';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: Config.SESSION_EXPIRES_AFTER_SECONDS },
+    }),],
 })
 export class AppModule {}
--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,3 +1,9 @@
 export class Config {
   public static readonly PASSWORD_MIN_LENGTH = 8;
+
+  /**
+   * @default
+   * 1 day
+   */
+  public static readonly SESSION_EXPIRES_AFTER_SECONDS = 60 * 60 * 24;
 }
