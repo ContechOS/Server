5c2401e2e7a2f6b3108874fd75eda7b3ddbae17d
Alex Sandri <me@alexsandri.com>
2021-08-31 11:46:24 +0200

    Fix: date serialization

--- src/utilities/Utilities.ts
+++ src/utilities/Utilities.ts
@@ -1,14 +1,7 @@
 import { DateTime } from "neo4j-driver";
 
 export class Utilities {
-  public static neo4jDateTimeToDateObject({ year, month, day, hour, minute, second }: DateTime): Date {
-    return new Date(
-      year.toNumber(),
-      month.toNumber() - 1, // Months start at 0
-      day.toNumber(),
-      hour.toNumber(),
-      minute.toNumber(),
-      second.toNumber(),
-    );
+  public static neo4jDateTimeToDateObject({ year, month, day, hour, minute, second, nanosecond }: DateTime): Date {
+    return new Date(`${year.toNumber()}-${month.toNumber().toString(10).padStart(2, '0')}-${day.toNumber().toString(10).padStart(2, '0')}T${hour.toNumber().toString(10).padStart(2, '0')}:${minute.toNumber().toString(10).padStart(2, '0')}:${second.toNumber().toString(10).padStart(2, '0')}.${nanosecond.div(1000).toNumber().toString(10)}Z`);
   }
 }
