f38b856b1f506efc1d77870145c4052f820356d7
Alex Sandri <me@alexsandri.com>
2021-08-31 13:36:27 +0200

    Fix: sign in mutation not returning token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { compare } from 'bcrypt';
-import { User } from 'src/users/entities/user.entity';
+import { randomBytes } from 'crypto';
 import { UsersService } from 'src/users/users.service';
 import { SignInInput } from './dto/sign-in.input';
+import { Auth } from './entities/auth.entity';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
-  async signIn(signInInput: SignInInput): Promise<User | null> {
+  async signIn(signInInput: SignInInput): Promise<Auth | null> {
     const user = await this.usersService.findOneByEmail(signInInput.email);
 
     if (!user) {
@@ -24,6 +25,9 @@ export class AuthService {
       return null;
     }
 
-    return user;
+    return new Auth({
+      user,
+      token: randomBytes(30).toString("hex"),
+    });
   }
 }
