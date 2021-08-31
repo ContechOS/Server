8d5f87c3b2ed086dabec7244230d8781bb7ee8ac
alex-sandri <alex-sandri@users.noreply.github.com>
2021-08-31 10:19:19 +0000

    Apply formatting changes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { SignInInput } from './dto/sign-in.input';
 
 @Injectable()
 export class AuthService {
-  constructor (private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
   async signIn(signInInput: SignInInput): Promise<User | null> {
     const user = await this.usersService.findOneByEmail(signInInput.email);
