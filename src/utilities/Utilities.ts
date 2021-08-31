99684738f44b2ef42cabeb854ac742e2ecaf443a
Alex Sandri <me@alexsandri.com>
2021-08-31 11:42:23 +0200

    fix: wrong month and day

--- src/utilities/Utilities.ts
+++ src/utilities/Utilities.ts
@@ -4,7 +4,7 @@ export class Utilities {
   public static neo4jDateTimeToDateObject({ year, month, day, hour, minute, second }: DateTime): Date {
     return new Date(
       year.toNumber(),
-      month.toNumber(),
+      month.toNumber() - 1, // Months start at 0
       day.toNumber(),
       hour.toNumber(),
       minute.toNumber(),
