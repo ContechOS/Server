f949736a8a3ebaba076a62a9a947140a45b605d5
Alex Sandri <me@alexsandri.com>
2021-09-01 08:41:23 +0200

    Create currentUser query

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,14 +1,23 @@
-import { Resolver, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Mutation, Args, Query } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { Auth } from './entities/auth.entity';
 import { SignInInput } from './dto/sign-in.input';
 import { UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './guards/local.guard';
 import { CreateUserInput } from 'src/users/dto/create-user.input';
+import { JwtAuthGuard } from './guards/jwt.guard';
+import { User } from 'src/users/entities/user.entity';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Resolver(() => Auth)
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
+
+  @Query(() => User, { name: "currentUser" })
+  @UseGuards(JwtAuthGuard)
+  currentUser(@CurrentUser() user: User) {
+    return user;
+  }
 
   @Mutation(() => Auth)
   @UseGuards(LocalAuthGuard)
