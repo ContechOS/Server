710d03ce7b41e379c531e0483160fccccb1a82ca
Alex Sandri <me@alexsandri.com>
2021-09-01 10:02:17 +0200

    Use .fromEnv method for Neo4jModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { Neo4jConfig, Neo4jModule } from 'nest-neo4j';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Neo4jModule } from 'nest-neo4j';
+import { ConfigModule } from '@nestjs/config';
 import Joi from 'joi';
 import { GraphQLModule } from '@nestjs/graphql';
 
@@ -15,24 +15,15 @@ import { GraphQLModule } from '@nestjs/graphql';
       isGlobal: true,
       validationSchema: Joi.object({
         JWT_SECRET: Joi.string().required(),
+        NEO4J_SCHEME: Joi.string().required(),
         NEO4J_HOST: Joi.string().required(),
         NEO4J_PORT: Joi.number().port().required(),
-        NEO4J_USER: Joi.string().required(),
-        NEO4J_PASS: Joi.string().required(),
+        NEO4J_USERNAME: Joi.string().required(),
+        NEO4J_PASSWORD: Joi.string().required(),
+        NEO4J_DATABASE: Joi.string().required(),
       }),
     }),
-    Neo4jModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService): Neo4jConfig => {
-        return {
-          scheme: 'neo4j',
-          host: configService.get('NEO4J_HOST')!,
-          port: configService.get('NEO4J_PORT')!,
-          username: configService.get('NEO4J_USER')!,
-          password: configService.get('NEO4J_PASS')!,
-        };
-      },
-    }),
+    Neo4jModule.fromEnv(),
     UsersModule,
     AuthModule,
   ],
